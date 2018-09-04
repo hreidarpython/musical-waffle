fb3c5acb02a443272cab3d306228b0671cb6ee9e
hreidarpython <hreidarthor@gmail.com>
2018-09-04 11:18:28 +0000

    max_int.py

--- max_int.py
+++ max_int.py
@@ -1,4 +1,10 @@
 
 num_int = int(input("Input a number: "))    # Do not change this line
 # Fill in the missing code
+for max_num in num_int:
+    if num > 0:
+        print("Error")
+    banani
+
+
 print("The maximum is", max_int)    # Do not change this line
